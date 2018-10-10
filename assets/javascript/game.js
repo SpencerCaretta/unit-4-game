f7ee43966c499d2a40a181ea663b014fafbafe52
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-10 18:31:43 -0500

    wrote notes for javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//Random number generated and pushed to Target-Score id
+//Assign values of each crystal randomly
+//If total is over Target-Score you Lose, add 1 to lose counter
+//If total is = Target-Score you win, add 1 to win counter
